53647354a7f4cb7664184537935d3edbab6c6cb0
Jack Hsiao <chhsiao@gms.tcu.edu.tw>
2024-11-01 15:54:52 +0800

    Add files via upload

---
+++ Cookie.js
@@ -0,0 +1,31 @@
+﻿// 設定 cookie
+function setCookie(name, value, days) {
+    const encodedValue = encodeURIComponent(value);
+    let expires = "";
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + encodedValue + expires + "; path=/";
+  }
+  
+  // 讀取 cookie
+  function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) {
+        return decodeURIComponent(c.substring(nameEQ.length, c.length));
+      }
+    }
+    return null;
+  }
+  
+  // 使用範例
+  /*
+  setCookie("example", "含有特殊字符的值，例如中文或空格", 7);
+  console.log(getCookie("example"));
+  */
---
+++ setting.js
@@ -0,0 +1,8 @@
+var FHIRrootURL ="https://hapi.fhir.org/baseR4/";
+
+// var FHIRrootURL = "https://fhir.tcumi.com:58443/r5/fhir/"
+//var FHIRrootURL = "https://fhir.dicom.tw/fhir/";
+
+// var FHIRrootURL = "https://hapi.fhir.tw/fhir/"
+
+ 
